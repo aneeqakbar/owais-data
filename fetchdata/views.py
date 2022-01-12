ecec55d291f2328c99a9db8788284169d4c9d38b
aneeqakbar <aneeqakbqr@gmail.com>
2022-01-12 13:13:30 +0500

    fixed bot flow (frontend)

--- fetchdata/views.py
+++ fetchdata/views.py
@@ -28,22 +28,23 @@ class SheetDataView(View):
     def get(self, request, slug, sheet_num):
         print(slug)
         print(sheet_num)
-        scraper_name = [scraper_name[0] for scraper_name in ScraperData.SCRAPER_NAMES if scraper_name[1] == slug]
-        if not scraper_name:
-            raise Http404()
-        queryset = ScraperData.objects.filter(scraper_name=scraper_name[0])
+        return render(request, "core/tebular_data_login.html")
+        # scraper_name = [scraper_name[0] for scraper_name in ScraperData.SCRAPER_NAMES if scraper_name[1] == slug]
+        # if not scraper_name:
+        #     raise Http404()
+        # queryset = ScraperData.objects.filter(scraper_name=scraper_name[0])
 
-        crypto_currency = request.GET.get('crypto_currency', None)
-        ticker = request.GET.get('ticker', None)
+        # crypto_currency = request.GET.get('crypto_currency', None)
+        # ticker = request.GET.get('ticker', None)
 
-        if crypto_currency:
-            queryset = queryset.filter(cryptocurrency__icontains=crypto_currency)
-        if ticker:
-            queryset = queryset.filter(ticker__icontains=ticker)
+        # if crypto_currency:
+        #     queryset = queryset.filter(cryptocurrency__icontains=crypto_currency)
+        # if ticker:
+        #     queryset = queryset.filter(ticker__icontains=ticker)
 
-        paginator = Paginator(queryset, 25)
-        page_number = request.GET.get('page')
-        data = paginator.get_page(page_number)
-        context = {"data": data}
-        return render(request, "core/tebular_data.html", context)
+        # paginator = Paginator(queryset, 25)
+        # page_number = request.GET.get('page')
+        # data = paginator.get_page(page_number)
+        # context = {"data": data}
+        # return render(request, "core/tebular_data.html", context)
 
