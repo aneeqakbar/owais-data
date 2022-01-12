8aa5f1231dfa6cc261f6dc8f8e8ef59f42330bdd
aneeqakbar <aneeqakbqr@gmail.com>
2022-01-12 17:58:24 +0500

    fixed auth flow in data view

--- fetchdata/urls.py
+++ fetchdata/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name="HomeView"),
     path('scraper/<slug:slug>/', views.ScraperView.as_view(), name="ScraperView"),
     path('scraper/<slug:slug>/<int:sheet_num>/', views.SheetDataView.as_view(), name="SheetDataView"),
+    path('scraper_data/<slug:slug>/<int:sheet_num>/', views.ScraperDataView.as_view(), name="ScraperDataView"),
 ]
--- fetchdata/views.py
+++ fetchdata/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import authenticate, login
 from django.http.response import Http404, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic import TemplateView
 from django.views import View
 from .filters import ScraperFilter
@@ -26,6 +26,34 @@ class ScraperView(View):
         context = {'scraper_name': slug}
         return render(request, "core/scraper_detail.html", context)
 
+
+class ScraperDataView(View):
+    def get(self, request, slug, sheet_num):
+        print(request.user)
+        print(request.user.is_authenticated)
+        if not request.user.is_authenticated:
+            return redirect("fetchdata:SheetDataView")
+
+        scraper_name = [scraper_name[0] for scraper_name in ScraperData.SCRAPER_NAMES if scraper_name[1] == slug]
+        if not scraper_name:
+            raise Http404()
+        queryset = ScraperData.objects.filter(scraper_name=scraper_name[0])
+
+        crypto_currency = request.GET.get('crypto_currency', None)
+        ticker = request.GET.get('ticker', None)
+
+        if crypto_currency:
+            queryset = queryset.filter(cryptocurrency__icontains=crypto_currency)
+        if ticker:
+            queryset = queryset.filter(ticker__icontains=ticker)
+
+        paginator = Paginator(queryset, 25)
+        page_number = request.GET.get('page')
+        data = paginator.get_page(page_number)
+        context = {"data": data}
+        return render(request, "core/tebular_data.html", context)
+
+
 class SheetDataView(View):
     def get(self, request, slug, sheet_num):
         print(slug)
@@ -39,25 +67,27 @@ class SheetDataView(View):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
+            context = {"slug": slug, "sheet_num": sheet_num}
+            return render(request, "core/scraper_settings.html", context)
 
-            scraper_name = [scraper_name[0] for scraper_name in ScraperData.SCRAPER_NAMES if scraper_name[1] == slug]
-            if not scraper_name:
-                raise Http404()
-            queryset = ScraperData.objects.filter(scraper_name=scraper_name[0])
+            # scraper_name = [scraper_name[0] for scraper_name in ScraperData.SCRAPER_NAMES if scraper_name[1] == slug]
+            # if not scraper_name:
+            #     raise Http404()
+            # queryset = ScraperData.objects.filter(scraper_name=scraper_name[0])
 
-            crypto_currency = request.GET.get('crypto_currency', None)
-            ticker = request.GET.get('ticker', None)
+            # crypto_currency = request.GET.get('crypto_currency', None)
+            # ticker = request.GET.get('ticker', None)
 
-            if crypto_currency:
-                queryset = queryset.filter(cryptocurrency__icontains=crypto_currency)
-            if ticker:
-                queryset = queryset.filter(ticker__icontains=ticker)
+            # if crypto_currency:
+            #     queryset = queryset.filter(cryptocurrency__icontains=crypto_currency)
+            # if ticker:
+            #     queryset = queryset.filter(ticker__icontains=ticker)
 
-            paginator = Paginator(queryset, 25)
-            page_number = request.GET.get('page')
-            data = paginator.get_page(page_number)
-            context = {"data": data}
-            return render(request, "core/tebular_data.html", context)
+            # paginator = Paginator(queryset, 25)
+            # page_number = request.GET.get('page')
+            # data = paginator.get_page(page_number)
+            # context = {"data": data}
+            # return render(request, "core/tebular_data.html", context)
 
         context = {"form": form}
         return render(request, "core/tebular_data_login.html", context)
